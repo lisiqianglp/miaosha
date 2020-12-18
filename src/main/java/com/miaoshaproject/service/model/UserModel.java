6587ee184b5a317693bba6fc928d08f8c3a20b50
常永宏 <>
2020-12-18 09:34:41 +0800

    no message

--- src/main/java/com/miaoshaproject/service/model/UserModel.java
+++ src/main/java/com/miaoshaproject/service/model/UserModel.java
@@ -4,7 +4,7 @@ public class UserModel {
     private Integer id;
     private String name;
     private Byte gender;
-    private String age;
+    private Integer age;
     private String telphone;
     private String registerMode;
 
@@ -22,7 +22,7 @@ public class UserModel {
         return gender;
     }
 
-    public String getAge() {
+    public Integer getAge() {
         return age;
     }
 
@@ -60,7 +60,7 @@ public class UserModel {
         this.gender = gender;
     }
 
-    public void setAge(String age) {
+    public void setAge(Integer age) {
         this.age = age;
     }
 
